1efaef3e308c0337adc5b0007ec477f566be6675
Diogo Matias <diogomaccedo200@gmail.com>
2023-07-04 17:12:03 -0300

    refactor: database config

--- src/main/config/database.ts
+++ src/main/config/database.ts
@@ -1,11 +1,14 @@
 export default {
     dialect: "postgres",
-    host: "localhost",
-    username: "",
-    password: "",
-    database: "diogomacedo",
+    host: "ep-withered-paper-622628-pooler.us-east-1.postgres.vercel-storage.com",
+    username: "default",
+    password: "VdJA0LoxgE3P",
+    database: "verceldb",
     define: {
         timestamps: true,
         underscored: true,
+    },
+    dialectOptions: {
+        ssl: true,
     },
 };
